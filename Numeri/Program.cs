e627b3212dfdd6b88d0abac30e9b05b64a05dda9
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-04-28 17:53:24 +0200

    Aggiungere i file di progetto.

---
+++ Numeri/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Corso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 2.0;
+            double c = a + b;
+            c = ++b; //b++ darebbe 2
+            a += 5;
+            int d = Int32.Parse("5");
+            Console.WriteLine(c);
+            int e = Math.Abs(-40);
+            double f = Math.Pow(5,2);
+        }
+    }
+}
